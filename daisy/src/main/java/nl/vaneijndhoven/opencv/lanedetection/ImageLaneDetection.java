d19802af68874ab679ad7c8bfd87721e340fdc7b
Tim van Eijndhoven <tvaneijndhoven@jpoint.nl>
2017-09-06 11:47:04 +0200

    Try to fix early release error

--- daisy/src/main/java/nl/vaneijndhoven/opencv/lanedetection/ImageLaneDetection.java
+++ daisy/src/main/java/nl/vaneijndhoven/opencv/lanedetection/ImageLaneDetection.java
@@ -11,7 +11,6 @@ import nl.vaneijndhoven.opencv.objectdetection.LineExtractor;
 import nl.vaneijndhoven.opencv.roi.RegionOfInterest;
 import nl.vaneijndhoven.opencv.stopzonedetection.DefaultStoppingZoneDetector;
 import nl.vaneijndhoven.opencv.tools.ImageCollector;
-import nl.vaneijndhoven.opencv.tools.MemoryManagement;
 import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
 
@@ -19,7 +18,6 @@ import java.util.*;
 
 import static java.util.Arrays.asList;
 import static nl.vaneijndhoven.opencv.mapper.PointMapper.toPoint;
-import static nl.vaneijndhoven.opencv.tools.MemoryManagement.closable;
 
 public class ImageLaneDetection {
 
@@ -32,61 +30,63 @@ public class ImageLaneDetection {
     }
 
     public Map<String, Object> detectLane(Mat original, ImageCollector imageCollector) {
-        try (MemoryManagement.ClosableMat<Mat> clsImage = closable(new RegionOfInterest(0, 0.45, 1, 0.55).region(original))) {
-            Mat image = clsImage.get();
-            Size imageSize = image.size();
-            ViewPort viewPort = new ViewPort(new Point(0, 0), imageSize.width, imageSize.height);
-
-            LineExtractor lineExtractor = new LineExtractor(
-                    new CannyEdgeDetector(cannyConfig).withImageCollector(imageCollector),
-                    new ProbabilisticHoughLinesLineDetector(lineDetectorConfig).withImageCollector(imageCollector)
-            );
-
-            Collection<Line> lines = lineExtractor.extract(image);
-
-            Lane lane = new DefaultLaneDetector().detect(lines, viewPort);
-            StoppingZone stoppingZone = new DefaultStoppingZoneDetector().detect(lines);
-
-            LaneOrientation laneOrientation = new LaneOrientation(lane, viewPort);
-            StoppingZoneOrientation stoppingZoneOrientation = new StoppingZoneOrientation(stoppingZone, lane, viewPort);
-
-            Optional<Line> middle = laneOrientation.determineLaneMiddle();
-
-            lane.getLeftBoundary().ifPresent(boundary -> drawLinesToImage(image, asList(boundary), new Scalar(0, 255, 0)));
-            lane.getRightBoundary().ifPresent(boundary -> drawLinesToImage(image, asList(boundary), new Scalar(255, 128, 0)));
-            middle.ifPresent(line -> drawLinesToImage(image, asList(line), new Scalar(0, 0, 255)));
-
-            double distanceToStoppingZone = -1;
-            double distanceToStoppingZoneEnd = -1;
-            if (stoppingZone.getEntrance() != null) {
-                stoppingZone.getEntrance().ifPresent(entrance -> drawLinesToImage(image, asList(entrance), new Scalar(255, 255, 0)));
-                distanceToStoppingZone = stoppingZoneOrientation.determineDistanceToStoppingZone();
-            }
-
-            if (stoppingZone.getExit() != null) {
-                stoppingZone.getExit().ifPresent(exit -> drawLinesToImage(image, asList(exit), new Scalar(0, 255, 255)));
-                distanceToStoppingZoneEnd = stoppingZoneOrientation.determineDistanceToStoppingZoneEnd();
-            }
-
-            imageCollector.lines(image);
-
-            double angle = laneOrientation.determineCurrentAngle();
-
-            double distanceMiddle = laneOrientation.determineDistanceToMiddle();
-            double distanceLeft = laneOrientation.distanceFromLeftBoundary();
-            double distanceRight = laneOrientation.distanceFromRightBoundary();
-
-            Map result = new HashMap<>();
-            result.put("lane", lane);
-            putIfNumber("angle", angle, result);
-            putIfNumber("distanceMiddle", distanceMiddle, result);
-            putIfNumber("distanceLeft", distanceLeft, result);
-            putIfNumber("distanceRight", distanceRight, result);
-            putIfNumber("distanceToStoppingZone", distanceToStoppingZone, result);
-            putIfNumber("distanceToStoppingZoneEnd", distanceToStoppingZoneEnd, result);
-
-            return result;
+        if (original.empty()) {
+            System.err.println("detectLane: empty mat?");
         }
+
+        Mat image = new RegionOfInterest(0, 0.45, 1, 0.55).region(original);
+        Size imageSize = image.size();
+        ViewPort viewPort = new ViewPort(new Point(0, 0), imageSize.width, imageSize.height);
+
+        LineExtractor lineExtractor = new LineExtractor(
+                new CannyEdgeDetector(cannyConfig).withImageCollector(imageCollector),
+                new ProbabilisticHoughLinesLineDetector(lineDetectorConfig).withImageCollector(imageCollector)
+        );
+
+        Collection<Line> lines = lineExtractor.extract(image);
+
+        Lane lane = new DefaultLaneDetector().detect(lines, viewPort);
+        StoppingZone stoppingZone = new DefaultStoppingZoneDetector().detect(lines);
+
+        LaneOrientation laneOrientation = new LaneOrientation(lane, viewPort);
+        StoppingZoneOrientation stoppingZoneOrientation = new StoppingZoneOrientation(stoppingZone, lane, viewPort);
+
+        Optional<Line> middle = laneOrientation.determineLaneMiddle();
+
+        lane.getLeftBoundary().ifPresent(boundary -> drawLinesToImage(image, asList(boundary), new Scalar(0, 255, 0)));
+        lane.getRightBoundary().ifPresent(boundary -> drawLinesToImage(image, asList(boundary), new Scalar(255, 128, 0)));
+        middle.ifPresent(line -> drawLinesToImage(image, asList(line), new Scalar(0, 0, 255)));
+
+        double distanceToStoppingZone = -1;
+        double distanceToStoppingZoneEnd = -1;
+        if (stoppingZone.getEntrance() != null) {
+            stoppingZone.getEntrance().ifPresent(entrance -> drawLinesToImage(image, asList(entrance), new Scalar(255, 255, 0)));
+            distanceToStoppingZone = stoppingZoneOrientation.determineDistanceToStoppingZone();
+        }
+
+        if (stoppingZone.getExit() != null) {
+            stoppingZone.getExit().ifPresent(exit -> drawLinesToImage(image, asList(exit), new Scalar(0, 255, 255)));
+            distanceToStoppingZoneEnd = stoppingZoneOrientation.determineDistanceToStoppingZoneEnd();
+        }
+
+        imageCollector.lines(image);
+
+        double angle = laneOrientation.determineCurrentAngle();
+
+        double distanceMiddle = laneOrientation.determineDistanceToMiddle();
+        double distanceLeft = laneOrientation.distanceFromLeftBoundary();
+        double distanceRight = laneOrientation.distanceFromRightBoundary();
+
+        Map result = new HashMap<>();
+        result.put("lane", lane);
+        putIfNumber("angle", angle, result);
+        putIfNumber("distanceMiddle", distanceMiddle, result);
+        putIfNumber("distanceLeft", distanceLeft, result);
+        putIfNumber("distanceRight", distanceRight, result);
+        putIfNumber("distanceToStoppingZone", distanceToStoppingZone, result);
+        putIfNumber("distanceToStoppingZoneEnd", distanceToStoppingZoneEnd, result);
+
+        return result;
     }
 
     private void putIfNumber(String key, double angle, Map result) {
