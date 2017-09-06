e85c8ddf712fc4ea939a26cd913cee776280fb13
Bert Jan Schrijver <bertjan@revolution.nl>
2017-09-06 11:39:03 +0200

    implement steering command processing

--- bo/src/main/java/nl/vaneijndhoven/dukes/bo/Bo.java
+++ bo/src/main/java/nl/vaneijndhoven/dukes/bo/Bo.java
@@ -28,33 +28,30 @@ public class Bo extends AbstractVerticle {
         Subscription subscription = vertx.eventBus().consumer(Characters.BO.getCallsign()).toObservable()
                 .doOnNext(x -> LOG.trace("Received instruction"))
                 .map(Message::body)
-                .cast(String.class)
-                .map(JsonObject::new)
-//                .map(msg -> new JsonObject(msg))
+                .cast(JsonObject.class)
                 .subscribe(message -> {
-//                    JsonObject message = (JsonObject)message.body();
-                        LOG.trace("Instruction: {}", message);
-//                    String type = message.getString("type");
-//                    switch (type) {
-//                        case "motor":
-//                            speedHandler.handleMotor(message);
-//                            break;
-//                        case "servo":
-//                            steeringHandler.handleServo(message);
-//                            break;
-//                        case "servoDirect":
-//                            steeringHandler.handleServoDirect(message);
-//                            break;
-//                        case "speedDirect":
-//                            speedHandler.handleSpeedDirect(message);
-//                            break;
-//                        case "log":
-//                            String logMessage = message.getString("message");
-//                            LOG.debug("Received log message: "  + logMessage);
-//                            break;
-//                        default:
-//                            LOG.error("Unknown message type {}", type);
-//                    }
+                    LOG.trace("Instruction: {}", message);
+                    String type = message.getString("type");
+                    switch (type) {
+                        case "motor":
+                            speedHandler.handleMotor(message);
+                            break;
+                        case "servo":
+                            steeringHandler.handleServo(message);
+                            break;
+                        case "servoDirect":
+                            steeringHandler.handleServoDirect(message);
+                            break;
+                        case "speedDirect":
+                            speedHandler.handleSpeedDirect(message);
+                            break;
+                        case "log":
+                            String logMessage = message.getString("message");
+                            LOG.debug("Received log message: "  + logMessage);
+                            break;
+                        default:
+                            LOG.error("Unknown message type {}", type);
+                    }
         });
 
         LOG.info("Bo started");
