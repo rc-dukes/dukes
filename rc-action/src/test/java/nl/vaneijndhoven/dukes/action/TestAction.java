a39d8f462d4be8b6f48417bd186c9bae43290b62
Wolfgang Fahl <wf@bitplan.com>
2019-07-12 19:56:58 +0100

    increase timeout factor to 60 to be even safer

--- rc-action/src/test/java/nl/vaneijndhoven/dukes/action/TestAction.java
+++ rc-action/src/test/java/nl/vaneijndhoven/dukes/action/TestAction.java
@@ -28,7 +28,7 @@ public class TestAction {
 		// in Travis environment things might be *really* slow
 		if (!TestSuite.isTravis()) {
 			// increase the timing
-			int TRAVIS_FACTOR = 37; // 50 worked - 25 did not
+			int TRAVIS_FACTOR = 60; // 50 worked - 25 and 37 did not
 			COMMAND_LOOP_INTERVAL = COMMAND_LOOP_INTERVAL * TRAVIS_FACTOR;
 			TIME_OUT = TIME_OUT * TRAVIS_FACTOR;
 		}
