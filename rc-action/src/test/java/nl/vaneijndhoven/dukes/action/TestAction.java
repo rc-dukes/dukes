a9295e0793ffc63551908d6aff3bbb021c9933fd
Wolfgang Fahl <wf@bitplan.com>
2019-07-12 17:05:58 +0100

    starts binary search for proper timeout factor

--- rc-action/src/test/java/nl/vaneijndhoven/dukes/action/TestAction.java
+++ rc-action/src/test/java/nl/vaneijndhoven/dukes/action/TestAction.java
@@ -28,7 +28,7 @@ public class TestAction {
 		// in Travis environment things might be *really* slow
 		if (!TestSuite.isTravis()) {
 			// increase the timing
-			int TRAVIS_FACTOR = 50;
+			int TRAVIS_FACTOR = 25; // 50 worked
 			COMMAND_LOOP_INTERVAL = COMMAND_LOOP_INTERVAL * TRAVIS_FACTOR;
 			TIME_OUT = TIME_OUT * TRAVIS_FACTOR;
 		}
