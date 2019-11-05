cb6802c21979c88a43a1025d7136b7ebffb7e255
Wolfgang Fahl <wf@bitplan.com>
2019-11-05 20:19:36 +0100

    handles null values as resulthandler result leading to vertx not being available

--- rc-common/src/main/java/nl/vaneijndhoven/dukes/common/ClusterStarter.java
+++ rc-common/src/main/java/nl/vaneijndhoven/dukes/common/ClusterStarter.java
@@ -18,76 +18,84 @@ import io.vertx.core.json.JsonObject;
  */
 public class ClusterStarter {
 
-	private VertxOptions options;
-	private boolean prepared = false;
-	private Vertx vertx;
+  private VertxOptions options;
+  private boolean prepared = false;
+  private Vertx vertx;
 
-	public Vertx getVertx() {
-		return vertx;
-	}
+  public Vertx getVertx() {
+    return vertx;
+  }
 
-	/**
-	 * prepare the starter
-	 */
-	public void prepare() {
-		if (!prepared) {
-			Config.configureLogging();
-			prepared = true;
-		}
-	}
+  /**
+   * prepare the starter
+   */
+  public void prepare() {
+    if (!prepared) {
+      Config.configureLogging();
+      prepared = true;
+    }
+  }
 
-	/**
-	 * 
-	 * @param resultHandler
-	 */
-	public void clusteredVertx(Handler<AsyncResult<Vertx>> resultHandler) {
-		prepare();
-		Vertx.clusteredVertx(getOptions(), resultHandler);
-	}
+  /**
+   * 
+   * @param resultHandler
+   */
+  public void clusteredVertx(Handler<AsyncResult<Vertx>> resultHandler) {
+    prepare();
+    Vertx.clusteredVertx(getOptions(), resultHandler);
+  }
 
-	/**
-	 * get the deployment options
-	 * @param worker - true if worker should be set to true
-	 * @return the deployment options
-	 * @throws Exception 
-	 */
-	public DeploymentOptions getDeployMentOptions(boolean worker) throws Exception {
-		DeploymentOptions deploymentOptions = new DeploymentOptions();
-		deploymentOptions.setWorker(worker);
-		JsonObject config=Config.getEnvironment().asJsonObject();
-		deploymentOptions.setConfig(config);
-		return deploymentOptions;
-	}
+  /**
+   * get the deployment options
+   * 
+   * @param worker
+   *          - true if worker should be set to true
+   * @return the deployment options
+   * @throws Exception
+   */
+  public DeploymentOptions getDeployMentOptions(boolean worker)
+      throws Exception {
+    DeploymentOptions deploymentOptions = new DeploymentOptions();
+    deploymentOptions.setWorker(worker);
+    JsonObject config = Config.getEnvironment().asJsonObject();
+    deploymentOptions.setConfig(config);
+    return deploymentOptions;
+  }
 
-	/**
-	 * deploy the given verticles
-	 * 
-	 * @param verticles
-	 * @throws Exception 
-	 */
-	public void deployVerticles(AbstractVerticle... verticles) throws Exception {
-		DeploymentOptions deploymentOptions = this.getDeployMentOptions(true);
-		this.clusteredVertx(resultHandler -> {
-			vertx = resultHandler.result();
-			for (AbstractVerticle verticle : verticles) {
-				try {
-					vertx.deployVerticle(verticle, deploymentOptions);
-				} catch (Throwable th) {
-					ErrorHandler.getInstance().handle(th);
-				}
-			}
-		});
-	}
+  /**
+   * deploy the given verticles
+   * 
+   * @param verticles
+   * @throws Exception
+   */
+  public void deployVerticles(AbstractVerticle... verticles) throws Exception {
+    DeploymentOptions deploymentOptions = this.getDeployMentOptions(true);
+    this.clusteredVertx(resultHandler -> {
+      vertx = resultHandler.result();
+      if (vertx != null) {
+        for (AbstractVerticle verticle : verticles) {
+          try {
+            vertx.deployVerticle(verticle, deploymentOptions);
+          } catch (Throwable th) {
+            ErrorHandler.getInstance().handle(th);
+          }
+        }
+      } else {
+        ErrorHandler.getInstance().handle(new RuntimeException("vertx not available resultHandler result is null"));
+      }
+    });
+  }
 
-	/**
-	 * get the VertexOptions
-	 * 
-	 * @return new Vertex Options with the HazelcastConfig from Config
-	 */
-	public VertxOptions getOptions() {
-		if (options == null) {
-			options = new VertxOptions().setClustered(true).setClusterManager(Config.createHazelcastConfig());
-		}
-		return options;
-	}
+  /**
+   * get the VertexOptions
+   * 
+   * @return new Vertex Options with the HazelcastConfig from Config
+   */
+  public VertxOptions getOptions() {
+    if (options == null) {
+      options = new VertxOptions().setClustered(true)
+          .setClusterManager(Config.createHazelcastConfig());
+    }
+    return options;
+  }
 }
