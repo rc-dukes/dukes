6c4f000064d7f365ad5bf3aa061806060af2cdbe
Wolfgang Fahl <wf@bitplan.com>
2020-01-28 17:32:42 +0100

    adds test suites

---
+++ rc-remotecar/src/test/java/org/rcdukes/remotecar/TestSuite.java
@@ -0,0 +1,25 @@
+package org.rcdukes.remotecar;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ TestRemoteCar.class })
+/**
+ * TestSuite for detect aka daisy
+ * 
+ * @author wf
+ *
+ *         no content necessary - annotation has info
+ */
+public class TestSuite {
+  /**
+   * check if we are in the Travis-CI environment
+   * 
+   * @return true if Travis user was detected
+   */
+  public static boolean isTravis() {
+    String user = System.getProperty("user.name");
+    return user.equals("travis");
+  }
+}
